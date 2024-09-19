a854ce61a9ca10831c66ed54dee08b650bb5d306
devpresss xmont <xmontemorjerald@gmail.com>
2024-09-19 20:26:26 +0800

    Added Nav bar

--- myrental/myrental/settings.py
+++ myrental/myrental/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myApp',
-    'debug_toolbar'
+    'debug_toolbar',
+    
 ]
 
 MIDDLEWARE = [
@@ -74,6 +75,9 @@ TEMPLATES = [
     },
 ]
 
+
+# Directory where static files will be collected to during deployment
+STATIC_ROOT = BASE_DIR / "staticfiles"
 WSGI_APPLICATION = 'myrental.wsgi.application'
 
 
@@ -128,3 +132,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "myApp" / "static",
+]
