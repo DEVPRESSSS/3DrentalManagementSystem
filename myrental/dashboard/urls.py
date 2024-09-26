355e37a3b26445fef34d2ef9d5e3ce31d9c5fa78
devpresss xmont <xmontemorjerald@gmail.com>
2024-09-27 00:37:40 +0800

    Added some designs and Crud operations in custom dashboard

--- myrental/dashboard/urls.py
+++ myrental/dashboard/urls.py
@@ -7,5 +7,9 @@ from .import views
 
 urlpatterns = [
     path("", views.dashboard, name="dashboard"),
+    path("overview/", views.overview, name="overview"),
+    path("tenants/", views.tenants, name="tenants"),
+    path("rental/", views.rental, name="rental"),
+     path("3Dmodels/", views.rental_model, name="3Dmodels"),
   
 ]
--- myrental/dashboard/views.py
+++ myrental/dashboard/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from myApp.models import *
 # Create your views here.
 def dashboard(request):
 
-    return render(request,"dashboard/base.html")
+    return render(request,"dashboard/dash_base.html")
+
+def overview(request):
+
+    return render(request,"dashboard/overview.html")
+
+def tenants(request):
+
+    tenants_list = Tenant.objects.all()
+
+    return render(request,"dashboard/tenants.html",{"tenants_list":tenants_list})
+
+def rental(request):
+    rental_list = RentalSpaces.objects.all()
+    return render(request,"dashboard/rentals.html",{"rental_list":rental_list})
+
+
+def rental_model(request):
+    rental_list_model = Model.objects.all()
+    print(rental_list_model)
+    return render(request,"dashboard/3Dmodels.html",{"rental_list_model":rental_list_model})
